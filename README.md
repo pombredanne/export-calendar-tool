e504384136db414308191bab6848a9f8199decd9
Zarino Zappia <mail@zarino.co.uk>
2013-04-24 22:45:11 +0100

    select boxes for column names

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ A ScraperWiki tool that lets you publish your dataset to an iCal feed.
 ## To do
 
 - -scraperwiki.json file.-
-- Get target dataset's metadata via /sql/meta endpoint.
+- -Get target dataset's metadata via /sql/meta endpoint.-
 - UI (select boxes) for matching dataset columns up with iCal event attributes.
+- Intelligent defaults for column name select boxes.
 - Advanced UI for suppyling a SQL query rather than just a column name.
 - Python script for generating iCal files, based on user settings.
 - List of previously generated files in UI.
--- http/code.js
+++ http/code.js
@@ -1,3 +1,22 @@
+settings = scraperwiki.readSettings()
+
 $(function(){
-  
+  $.ajax({
+    url: settings.target.url + '/sql/meta',
+    dataType: 'json',
+    success: function(data){
+      var html = '<option selected></option>'
+      $.each(data.table, function(tableName, t){
+        html += '<optgroup label="'+ tableName +'">'
+        $.each(t.columnNames, function(i, columnName){
+          html += '<option value="'+ tableName +'.'+ columnName +'">'+ columnName +'</option>'
+        })
+        html += '</optgroup>'
+      })
+      $('select').html(html)
+    },
+    error: function(jqXHR, textStatus, errorThrown){
+      console.log(jqXHR.responseText, textStatus, errorThrown)
+    }
+  })
 })
